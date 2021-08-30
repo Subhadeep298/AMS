409b576695fdb9a616993d42c4c709ef49fb7726
Soumyajit Podder <poddersoumyajit@gmail.com>
2021-08-31 01:02:24 +0530

    delete updated

--- ams.py
+++ ams.py
@@ -301,12 +301,12 @@ def DisplayData():
 def Delete():
     selected_item = tree.selection()[0]
     tree.delete(selected_item)
-    ''' conn = sqlite3.connect('ad.db')
+    conn = sqlite3.connect('ad.db')
     c = conn.cursor()
-    c.execute("DELETE from AD_MANAGE WHERE oid="+ selected_item)
-    delete_box.delete(0, END)
+    c.execute("DELETE from AD_MANAGE WHERE oid="+ selected_item[-1])
+    #delete_box.delete(0, END)
     conn.commit()
-    conn.close() '''
+    conn.close()
 
 
 DisplayForm()
